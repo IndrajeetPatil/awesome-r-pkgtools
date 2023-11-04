dcdab74945ebb778634509b9d7ac3d32c6191714
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-11-04 08:47:23 +0100

    render both md and html

--- renderer.R
+++ renderer.R
@@ -1,5 +1,5 @@
 # if you want to build the README locally
-quarto::quarto_render("README.qmd", output_format = "html")
+quarto::quarto_render("README.qmd", output_format = "all")
 
 # duplicate `README.html` as `index.html`
 fs::file_copy("README.html", "index.html", overwrite = TRUE)
