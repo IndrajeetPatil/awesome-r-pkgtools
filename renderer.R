50d0a8b0b8bbb694dfed57d5d6450cd9a3cc27ae
Indrajeet Patil <patilindrajeet.science@gmail.com>
2025-10-12 14:17:10 +0200

    cleanup

--- renderer.R
+++
@@ -1,6 +0,0 @@
-# if you want to build the README locally
-quarto::quarto_render("README.qmd", output_format = "all")
-
-# duplicate `README.html` as `index.html`
-fs::file_copy("README.html", "index.html", overwrite = TRUE)
-fs::file_delete("README.html")
